2ac0f0172125ca3bdad6cee56b45ebc479210873
benjamin-voca <benjaminvocaa@gmail.com>
2024-12-30 00:32:21 +0100

    idx

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -14,13 +14,13 @@ val tauriProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 30
     namespace = "com.hermes.app"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "com.hermes.app"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 30
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
