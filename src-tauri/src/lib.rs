df456718b56ab07898843cbc83846108d809d305
benjamin-voca <benjaminvocaa@gmail.com>
2025-01-10 00:41:07 +0100

    still stuck

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn add2() -> i32 {
+    2
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, add2])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/Foreign/Tauri.purs
+++ src/Foreign/Tauri.purs
@@ -1,11 +1,12 @@
 module Conduit.Foreign.Tauri where
 
+import Data.Either (Either)
+import Effect.Aff (Aff)
 import Foreign (Foreign)
 import Foreign.Object as FO
-import Promise (Promise)
 
 -- | Invoke a Tauri command with the given name and arguments.
 foreign import invokeTauriCommand
   :: String
-  -> -- Command name
-  (FO.Object Foreign -> Promise Foreign) -- Function taking arguments and returning a Promise
+  -> FO.Object Foreign
+  -> Aff (Either String Foreign) -- Aff to handle async effects with error handling
