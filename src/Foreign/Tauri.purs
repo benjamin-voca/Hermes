336a62a36b0a7691557687830683b4de47b55df6
benjamin-voca <benjaminvocaa@gmail.com>
2025-01-10 01:10:57 +0100

    progress

--- src/Foreign/Tauri.purs
+++ src/Foreign/Tauri.purs
@@ -1,12 +1,30 @@
 module Conduit.Foreign.Tauri where
 
-import Data.Either (Either)
-import Effect.Aff (Aff)
+import Prelude
+
+import Data.Either (Either(..))
+import Effect.Aff (error)
+import Effect.Aff.Compat (EffectFnAff)
+import Effect.Exception (throw)
 import Foreign (Foreign)
+import Foreign (readInt)
+import Foreign.Object (Object)
 import Foreign.Object as FO
 
 -- | Invoke a Tauri command with the given name and arguments.
 foreign import invokeTauriCommand
   :: String
   -> FO.Object Foreign
-  -> Aff (Either String Foreign) -- Aff to handle async effects with error handling
+  -> EffectFnAff Foreign
+
+-- | Decodes a Foreign value to an Int, throwing an error if it fails.
+unsafeDecodeInt :: Foreign -> Int
+unsafeDecodeInt value = case readInt value of
+  Right n -> n
+  Left err -> throw $ error $ "Expected Int, got: " <> show err
+
+-- | Returns an Int from the "add2" Tauri command.
+add2 :: EffectFnAff Int
+add2 = do
+  result <- invokeTauriCommand "add2" FO.empty
+  pure $ unsafeDecodeInt result
